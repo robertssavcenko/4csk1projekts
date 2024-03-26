c6da52046e94a16f905fecb6c03c450616ab5c88
robertssavcenko <roberts.savcenko@gmail.com>
2024-03-26 12:42:33 +0200

    pirmas stundas

---
+++ main.py
@@ -0,0 +1,14 @@
+pyhton -m pip install Flask
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "sveiki"
+
+if __name__ == '__main__':
+app.run(port = 5000)
+
+print("sveiki")
