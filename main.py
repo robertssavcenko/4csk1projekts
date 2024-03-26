d83b5f7e9596ab53cddfa5757d44a95d1c111155
robertssavcenko <roberts.savcenko@gmail.com>
2024-03-26 12:49:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-<<<<<<< HEAD
-pyhton -m pip install Flask
+
 
 from flask import Flask
-=======
-
-from flask import Flask, render_template
->>>>>>> 2f7458e63bfeacd00c16db35107c41979c8bac2f
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-<<<<<<< HEAD
-    return "sveiki"
+    return render_template("index.html")
+
+@app.route("/teksts")
+def teksts():
+    return render_template("teksts.html")
 
 if __name__ == '__main__':
 app.run(port = 5000)
 
 print("sveiki")
-=======
-    return render_template("index.html")
-
-@app.route("/teksts")
-def te():
-    return render_template("teksts.html")
-
-if __name__ == '__main__':
-    app.run(port=5000)
-
-    print ("sveiki")
->>>>>>> 2f7458e63bfeacd00c16db35107c41979c8bac2f
