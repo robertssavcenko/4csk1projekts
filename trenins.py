0bddece83bf4fea3c2eb508d2a34cbfddf8a004c
robertssavcenko <roberts.savcenko@gmail.com>
2024-03-27 12:59:02 +0200

    26,27 marta darbs

--- trenins.py
+++ trenins.py
@@ -3,17 +3,24 @@ def rezultats(sk1, sk2):
         rez = sk1*sk2
     else:
         rez = sk1+sk2
-        return rez
-    
-for skaitlis in range(1, 11, 2): #range funkcija kas skaita skaitlus
-    for skaitlis2 in range(1, 11, 2):
-        print("mūsu skaitlis:", skaitlis, "rezultāts:", rezultats(4, skaitlis))
+
+    return rez
+
+for skaitlis in range(1, 11, 2):        #range - funkcija, kas skaita skaitļus
+    for otrs in range(2, 11, 2):
+        print("pirmais skaitlis:", skaitlis,"otrais skaitlis:", otrs, "rezultāts: ", rezultats(skaitlis, otrs))
+
 
 def zvaigznites1(skaits):
     for rindasNr in range(1, skaits+1):
         for zvaigzne in range(rindasNr):
-            print("*")
+            print("*", end="")
+        print("")
+
+def zvaigznites2(skaits):
+    for rindasNr in range(1, skaits+1):
         print("*"*rindasNr)
+
 
 zvaigznites1(7)
 
